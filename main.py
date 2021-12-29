2172070d8ac9c66cfeac0926d5d2c14ec9dc0e8c
Pixelgon <pixelgon4@gmail.com>
2021-12-29 18:38:55 +0100

    Waiting for mouse left click

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import turtle
-
-def setCrds(event):
-    global x, y
-    x = event.x
-    y = event.y
+import turtle, pyautogui
+turtle.title("turtlePaint")
 wn = turtle.Screen()
 wn.setup(1200, 1200)
 screen = wn.getcanvas()
 t = turtle.Turtle()
-x = 600
-y = 600
-screen.bind('<Motion>', setCrds)
 t.speed('fastest')
 turtle.tracer(0, 0)
 t.hideturtle()
-turtle.title("turtlePaint")
+x = 600
+y = 600
+t.penup()
+def paint(x, y):
+    t.pendown()
+
+turtle.onscreenclick(paint)
+def setCrds(event):
+    global x, y
+    x = event.x
+    y = event.y
+screen.bind('<Motion>', setCrds)
 while True:
     t.setposition(x-600, (y*-1)+600)
     turtle.update()
