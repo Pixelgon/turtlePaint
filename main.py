6631de9fec6dbe6fbfad5af66ad7c53e71f2b987
Pixelgon <pixelgon4@gmail.com>
2021-12-30 04:35:56 +0100

    Waiting for mouse release left btn

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ wn = turtle.Screen()
 wn.setup(1200, 1200)
 screen = wn.getcanvas()
 t = turtle.Turtle()
+write = turtle.Turtle()
 t.speed('fastest')
+write.speed('fastest')
 turtle.tracer(0, 0)
 t.hideturtle()
 x = 600
 y = 600
 t.penup()
+write.shape('circle')
+write.shapesize(0.2, 0.2, 1)
+write.penup()
 def paint(x, y):
     t.pendown()
+def move(x, y):
+    t.penup()
 
-turtle.onscreenclick(paint)
+write.onclick(paint)
+write.onrelease(move)
 def setCrds(event):
     global x, y
     x = event.x
@@ -21,4 +29,5 @@ def setCrds(event):
 screen.bind('<Motion>', setCrds)
 while True:
     t.setposition(x-600, (y*-1)+600)
+    write.setposition(x-600, (y*-1)+600)
     turtle.update()
